60631685c42622b7085addf9e10d6bc157d0b119
wicsay <124921503+ralina16@users.noreply.github.com>
2025-04-15 19:36:01 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,21 +16,28 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    // Выпадающее меню
-    const dropdownButton = document.getElementById('dropdownButton');
-    const dropdownMenu = document.getElementById('dropdownMenu');
+   // Выпадающее меню
+const dropdownButton = document.getElementById('dropdownButton');
+const dropdownMenu = document.getElementById('dropdownMenu');
 
-    if (dropdownButton && dropdownMenu) {
-        dropdownButton.addEventListener('click', () => {
-            dropdownMenu.classList.toggle('active');
-        });
+if (dropdownButton && dropdownMenu) {
+    dropdownButton.addEventListener('click', () => {
+        dropdownMenu.classList.toggle('active');
+    });
 
-        document.addEventListener('click', (event) => {
-            if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
-                dropdownMenu.classList.remove('active');
-            }
+    document.addEventListener('click', (event) => {
+        if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            dropdownMenu.classList.remove('active');
+        }
+    });
+
+    const dropdownItems = dropdownMenu.querySelectorAll('.dropdown-item-dark');
+    dropdownItems.forEach(item => {
+        item.addEventListener('click', () => {
+            dropdownMenu.classList.remove('active');
         });
-    }
+    });
+}
 
 });
 
@@ -78,23 +85,58 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+// Фильтр в каталоге
+document.addEventListener("DOMContentLoaded", () => {
+    const filterButtons = document.querySelectorAll('.btn-filter, .dropdown-item-dark');
+    const cards = document.querySelectorAll('.cat-cards [data-category]');
+
+    function filterCards(category) {
+        cards.forEach(card => {
+            if (category === 'all' || card.dataset.category === category) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    }
+
+    filterButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const selectedCategory = button.dataset.category;
+            filterCards(selectedCategory);
+        });
+    });
+
+    filterCards('all');
+});
 // index end
 
 // about start
 function toggleAnswer(question) {
-    const answer = question.nextElementSibling;
-    const icon = question.querySelector('.faq-icon');
+    const allAnswers = document.querySelectorAll('.faq-answer');
+    const allIcons = document.querySelectorAll('.faq-icon');
 
-    console.log('Scroll Height:', answer.scrollHeight); // Отладочный вывод
+    const currentAnswer = question.nextElementSibling;
+    const currentIcon = question.querySelector('.faq-icon');
 
-    if (answer.classList.contains('open')) {
-        answer.style.maxHeight = '0';
-        icon.classList.remove('rotate');
+    allAnswers.forEach((answer, index) => {
+        if (answer !== currentAnswer) {
+            answer.style.maxHeight = '0';
+            answer.classList.remove('open');
+            allIcons[index].classList.remove('rotate');
+        }
+    });
+
+    if (currentAnswer.classList.contains('open')) {
+        currentAnswer.style.maxHeight = '0';
+        currentIcon.classList.remove('rotate');
     } else {
-        answer.style.maxHeight = answer.scrollHeight + 'px';
-        icon.classList.add('rotate');
+        currentAnswer.style.maxHeight = currentAnswer.scrollHeight + 'px';
+        currentIcon.classList.add('rotate');
     }
 
-    answer.classList.toggle('open');
+    currentAnswer.classList.toggle('open');
 }
 // about end
